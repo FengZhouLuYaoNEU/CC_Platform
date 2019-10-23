bc2500dbd37887cf323aa719260ef7191b3a7199
ChaochaoLi <lichao694@126.com>
2019-10-23 12:02:00 +0800

    [task] add timestamp to str func

--- platform/utils/include/CCTimeInfo.h
+++ platform/utils/include/CCTimeInfo.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <ctime>
 #include <cstdint>
+#include <string>
 using std::uint32_t;
 namespace CCPlatform
 {
@@ -13,4 +14,22 @@ namespace CCPlatform
     // Parameter: uint32_t year
     //************************************
     bool is_leap_year(uint32_t year);
+    //************************************
+    // Method:    ms_timestamp_to_str_date
+    // FullName:  CCPlatform::ms_timestamp_to_str_date
+    // Access:    public 
+    // Returns:   std::string
+    // Qualifier:
+    // Parameter: std::uint64_t timestamp
+    //************************************
+    std::string ms_timestamp_to_str_date(std::uint64_t timestamp);
+    //************************************
+    // Method:    us_timestamp_to_str_date
+    // FullName:  CCPlatform::us_timestamp_to_str_date
+    // Access:    public 
+    // Returns:   std::string
+    // Qualifier:
+    // Parameter: std::uint64_t timestamp
+    //************************************
+    std::string us_timestamp_to_str_date(std::uint64_t timestamp);
 }
--- platform/utils/source/CCTimeInfo.cpp
+++ platform/utils/source/CCTimeInfo.cpp
@@ -1,8 +1,35 @@
 #include "CCTimeInfo.h"
+#include <cstring>
 namespace CCPlatform
 {
     bool is_leap_year(uint32_t year)
     {
         return ((year % 400) == 0) || ((year % 4 == 0) && (year % 100 != 0));
+    }
+
+    std::string ms_timestamp_to_str_date(std::uint64_t timestamp)
+    {
+        std::time_t time_tick = (std::time_t)timestamp;
+        std::tm* time_info_ptr = std::localtime(&time_tick);
+        if (NULL == time_info_ptr) {
+            return std::string("");
+        }
+        char buf[128] = {0};
+        std::strftime(buf, sizeof(buf), "%Y-%m-%d:%H:%M:%S", time_info_ptr);
+        return std::string(buf);
+    }
+
+    std::string us_timestamp_to_str_date(std::uint64_t timestamp)
+    {
+        std::uint64_t micro_seconds = timestamp % 1000;
+        std::time_t time_tick = (std::time_t)(timestamp / 1000);
+        std::tm* time_info_ptr = std::localtime(&time_tick);
+        if (NULL == time_info_ptr) {
+            return std::string("");
+        }
+        char buf[128] = {0};
+        std::strftime(buf, sizeof(buf), "%Y-%m-%d:%H:%M:%S", time_info_ptr);
+        std::snprintf(buf + std::strlen(buf), 3, "%03lu", micro_seconds);
+        return std::string(buf);
     }
 }
